7a3e974fb52b48942db0b1b9506bdc31fe38180f
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2025-07-01 18:06:49 +0300

    Fix: use topologicalSortConfig for sorting attributes in dump

--- src/parsers/utils.js
+++ src/parsers/utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable sonarjs/cognitive-complexity */
 /* eslint-disable no-case-declarations */
 /* eslint-disable unicorn/no-array-reduce */
 /* eslint-disable no-param-reassign */
@@ -52,7 +53,7 @@ const operators = {
     round    : (args) => Math.round(args[0]),
     floor    : (args) => Math.floor(args[0]),
     ceil     : (args) => Math.ceil(args[0]),
-    divide   : (args) => args[1] !== 0 ? args[0] / args[1] : null,
+    divide   : (args) => (args[1] !== 0) ? args[0] / args[1] : null,
     multiply : (args) => args.reduce((a, b) => a * b, 1),
     add      : (args) => args.reduce((a, b) => a + b, 0),
     subtract : (args) => args.length === 1 ? -args[0] : args[0] - args[1],
@@ -62,6 +63,7 @@ const operators = {
 function evaluateNode(node, context, dumped) {
     switch (node.type) {
         case 'number':
+            return +getDeepValue(context, node.key);
         case 'string':
             return getDeepValue(context, node.key);
 
@@ -76,13 +78,72 @@ function evaluateNode(node, context, dumped) {
                 ? node.arguments.map(arg => evaluateNode(arg, context, dumped))
                 : [ evaluateNode(node.arguments, context, dumped) ];
 
-
             return fn(args);
 
         default:
             throw new Error(`Unknown node type: ${node.type}`);
     }
 }
+
+function extractDependencies(node) {
+    const deps = new Set();
+
+    function walk(n) {
+        if (n.type === 'dumped_value') {
+            deps.add(n.key);
+        } else if (n.type === 'function' && Array.isArray(n.arguments)) {
+            n.arguments.forEach((element) => {
+                walk(element);
+            });
+        }
+    }
+
+    walk(node);
+
+    return [ ...deps ];
+}
+
+function topologicalSortConfig(config) {
+    const graph = {};
+    const inDegree = {};
+
+    for (const [ key, node ] of Object.entries(config)) {
+        const deps = extractDependencies(node);
+
+        graph[key] = deps;
+        inDegree[key] = inDegree[key] || 0;
+
+        for (const dep of deps) {
+            inDegree[dep] = inDegree[dep] || 0;
+            inDegree[key]++;
+        }
+    }
+
+    const sorted = [];
+    const queue = Object.entries(inDegree)
+        .filter(([ , deg ]) => deg === 0)
+        .map(([ key ]) => key);
+
+    while (queue.length > 0) {
+        const current = queue.shift();
+
+        sorted.push(current);
+
+        for (const [ node, deps ] of Object.entries(graph)) {
+            if (deps.includes(current)) {
+                inDegree[node]--;
+                if (inDegree[node] === 0) queue.push(node);
+            }
+        }
+    }
+
+    if (sorted.length !== Object.keys(config).length) {
+        throw new Error('Cyclic dependency detected in config');
+    }
+
+    return sorted.map((key) => [ key, config[key] ]);
+}
+
 
 export function dump(item, config) {
     const paramMap = getParamMap(item.params || []);
@@ -97,7 +158,7 @@ export function dump(item, config) {
 
     const result = {};
 
-    for (const [ key, expr ] of Object.entries(config)) {
+    for (const [ key, expr ] of topologicalSortConfig(config)) {
         result[key] = evaluateNode(expr, context, result);
     }
 
